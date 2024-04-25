6a08a96c440e50d3f54ac6e72ca238b9b9f6fc29
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-25 20:34:20 +0000

    added image

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Fahrenheit to Celsius, in PHP" />
+  <meta name="description" content="Number Guessing Game, in PHP" />
   <meta name="keywords" content="mths, ics2o" />
   <meta name="author" content="Nathan De Silva" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -14,7 +14,7 @@
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Fahrenheit to Celsius, in PHP</title>
+  <title>Number Guessing Game, in PHP</title>
 </head>
 
 <body>
@@ -22,12 +22,12 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Convert Fahrenheit to Celsius, in PHP</span>
+        <span class="mdl-layout-title">Number Guessing Game, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/thermometer.png" alt="thermometer" width="250" />
+        <img src="./images/dice.gif" alt="dice" width="250" />
       </div>
       <h3>Convert Fahrenheit to Celsius:
         <br />
